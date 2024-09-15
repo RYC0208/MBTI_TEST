d194b64294d3ef7910f6aa02eb569c1e4e7584a8
RYC0208 <ryc0208@naver.com>
2024-09-16 08:02:13 +0900

    feat: persist 적용

--- mbti-test-app/src/zustand/authStore.js
+++ mbti-test-app/src/zustand/authStore.js
@@ -5,60 +5,71 @@ import {
   getUserProfile,
   updateProfile,
 } from "../api/auth";
+import { persist } from "zustand/middleware";
+//persist 로컬 스토리지 관리
 
-//persistStore 로컬 스토리지 관리
+const useAuthStore = create(
+  persist(
+    (set, get) => ({
+      user: JSON.parse(localStorage.getItem("user")),
+      isAuthenticated: !!localStorage.getItem("token"),
+      token: null,
+      profile: null,
 
-const useAuthStore = create((set, get) => ({
-  user: JSON.parse(localStorage.getItem("user")),
-  isAuthenticated: !!localStorage.getItem("token"),
-  token: null,
-  profile: null,
+      login: async (formData) => {
+        const response = await apiLogin(formData);
+        localStorage.setItem("token", response.accessToken);
+        localStorage.setItem("user", JSON.stringify(response));
+        set({
+          isAuthenticated: true,
+          token: response.accessToken,
+          user: response,
+        });
+        return response;
+      },
 
-  login: async (formData) => {
-    const response = await apiLogin(formData);
-    localStorage.setItem("token", response.accessToken);
-    localStorage.setItem("user", JSON.stringify(response));
-    set({ isAuthenticated: true, token: response.accessToken, user: response });
-    return response;
-  },
+      logout: () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        set({ isAuthenticated: false, user: null, token: null });
+      },
 
-  logout: () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    set({ isAuthenticated: false, user: null, token: null });
-  },
+      register: async (formData) => {
+        const response = await registerApi(formData);
+        return response;
+      },
 
-  register: async (formData) => {
-    const response = await registerApi(formData);
-    return response;
-  },
+      fetchProfile: async () => {
+        const { token, isAuthenticated } = get();
+        if (token && isAuthenticated) {
+          const userData = await getUserProfile(token);
+          set({ profile: userData });
+        }
+      },
 
-  fetchProfile: async () => {
-    const { token, isAuthenticated } = get();
-    if (token && isAuthenticated) {
-      const userData = await getUserProfile(token);
-      set({ profile: userData });
+      updateProfile: async (newNickname) => {
+        const { profile, token } = get();
+        const formData = new FormData();
+        formData.append("nickname", newNickname);
+
+        const response = await updateProfile(token, formData);
+        if (response.success) {
+          set({
+            profile: {
+              ...profile,
+              nickName: response.nickname,
+            },
+          });
+          return response.success;
+        } else {
+          return response.success;
+        }
+      },
+    }),
+    {
+      name: "allStorage",
     }
-  },
-
-  updateProfile: async (newNickname) => {
-    const { profile, token } = get();
-    const formData = new FormData();
-    formData.append("nickname", newNickname);
-
-    const response = await updateProfile(token, formData);
-    if (response.success) {
-      set({
-        profile: {
-          ...profile,
-          nickName: response.nickname,
-        },
-      });
-      return response.success;
-    } else {
-      return response.success;
-    }
-  },
-}));
+  )
+);
 
 export default useAuthStore;
